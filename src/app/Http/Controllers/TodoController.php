55de8c808a52ce64d07a726a16dcf5ea5bf949a3
YUKKE <gossip.girl.y1009@gmail.co>
2026-01-14 23:48:29 +0900

    Todo削除フォームをRESTful対応に修正

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
 use Illuminate\Http\Request;
 
@@ -10,13 +11,26 @@ class TodoController extends Controller
     public function index()
     {
         $todos = Todo::all();
+
         return view('index', compact('todos'));
     }
-
-    public function store(Request $request)
+    public function store(TodoRequest $request)
     {
         $todo = $request->only(['content']);
-        todo::create($todo);
-        return redirect('/');
+        Todo::create($todo);
+
+        return redirect('/')->with('message', 'Todoを作成しました');
+    }
+    public function update(TodoRequest $request)
+    {
+        $todo = $request->only(['content']);
+        Todo::find($request->id)->update($todo);
+
+        return redirect('/')->with('message', 'Todoを更新しました');
+    }
+    public function destroy(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/')->with('message', 'Todoを削除しました');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
-Route::POST('/todos', [TodoController::class, 'store']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
+Route::delete('/todos/delete', [TodoController::class, 'destroy']);
