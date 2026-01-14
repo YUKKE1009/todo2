dc7a6f02abe41f555078c92011c38851dfb4e28b
YUKKE <gossip.girl.y1009@gmail.co>
2026-01-14 22:24:59 +0900

    Todoの追加・更新機能実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
+Route::POST('/todos', [TodoController::class, 'store']);
